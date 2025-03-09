604ce7016e4155c9c9d5c3e0eb2cc54f274af15a
URANIUM <107063122+URANIUM11011@users.noreply.github.com>
2025-03-09 14:41:58 +0530

    Update start.py

--- AnieXEricaMusic/plugins/bot/start.py
+++ AnieXEricaMusic/plugins/bot/start.py
@@ -129,7 +129,7 @@ async def welcome(client, message: Message):
 
             if isinstance(config.OWNER_ID, int): 
                 if member.id == config.OWNER_ID:
-                    owner = f"#BOT_OWNER\n\n Stay Alert ⚠️\n\n{member.mention} Owner Of {app.mention} Just Joined The Group <code>{message.chat.title}</code>.\n\nSupport Me Here 👇🏻\n\n┏━━━━━━━━━━━━┓\n┣★ Updates -: @TheMenXD  \n┣★ Updates -: @Sargam_Updates \n┣★ Support -: @AM_YTSupport \n┣★ 𝗕𝗼𝘁 𝗨𝘀𝗲𝗿𝗡𝗮𝗺𝗲 -: @{app.username}\n┣★ 𝗦𝘂𝗽𝗲𝗿𝗯𝗮𝗻 𝗟𝗼𝗴𝘀 -: @SuperBanSBots\n┣★ 𝓐𝓫𝓸𝓾𝓽 𝓐𝓶𝓑𝓸𝓽 -: @AbouT_AMBoT\n┣★ 𝗳𝗲𝗱𝗲𝗿𝗮𝘁𝗶𝗼𝗻𝘀 𝗝𝗼𝗶𝗻 𝗛𝗲𝗿𝗲 -:<code> /joinfed f6c21c57-962c-4858-97a2-4d2f16cd68d6</code>\n┣★ 𝙉𝙤𝙩𝙚  -: 𝗧𝗵𝗶𝘀 𝗜𝘀 𝗢𝗻𝗹𝘆 𝗙𝗼𝗿 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗙𝗼𝗿 𝗠𝘆 𝗢𝘄𝗻𝗲𝗿 {member.mention}."
+                    owner = f"#BOT_OWNER\n\n Stay Alert ⚠️\n\n{member.mention} Owner Of {app.mention} Just Joined The Group <code>{message.chat.title}</code>.\n\nSupport Me Here 👇🏻\n\n┏━━━━━━━━━━━━┓\n┣★ Updates -: @TheMenXD  \n┣★ Updates -: @Sargam_Updates \n┣★ Support -: @II_NIGHT_HIGHS_II \n┣★ Bot Username  -: @{app.username}\n┣★ 𝗦𝘂𝗽𝗲𝗿𝗯𝗮𝗻 𝗟𝗼𝗴𝘀 -: @SuperBanSBots\n┣★ About Dev -: @The_Uranium\n┣★ 𝗳𝗲𝗱𝗲𝗿𝗮𝘁𝗶𝗼𝗻𝘀 𝗝𝗼𝗶𝗻 𝗛𝗲𝗿𝗲 -:<code> /joinfed f6c21c57-962c-4858-97a2-4d2f16cd68d6</code>\n┣★ 𝙉𝙤𝙩𝙚  -: 𝗧𝗵𝗶𝘀 𝗜𝘀 𝗢𝗻𝗹𝘆 𝗙𝗼𝗿 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗙𝗼𝗿 𝗠𝘆 𝗢𝘄𝗻𝗲𝗿 {member.mention}."
                     sent_message = await message.reply_text(owner, reply_markup=buttons)
                     await asyncio.sleep(20) 
                     await sent_message.delete()  
@@ -137,7 +137,7 @@ async def welcome(client, message: Message):
 
             elif isinstance(config.OWNER_ID, (list, set)): 
                 if member.id in config.OWNER_ID:
-                    owner = f"#BOT_OWNER\n\n Stay Alert ⚠️\n\n{member.mention} Owner Of {app.mention} Just Joined The Group <code>{message.chat.title}</code>.\n\nSupport Me Here 👇🏻\n\n┏━━━━━━━━━━━━┓\n┣★ Updates -: @TheMenXD  \n┣★ Updates -: @Sargam_Updates \n┣★ Support -: @AM_YTSupport \n┣★ 𝗕𝗼𝘁 𝗨𝘀𝗲𝗿𝗡𝗮𝗺𝗲 -: @{app.username}\n┣★ 𝗦𝘂𝗽𝗲𝗿𝗯𝗮𝗻 𝗟𝗼𝗴𝘀 -: @SuperBanSBots\n┣★ 𝓐𝓫𝓸𝓾𝓽 𝓐𝓶𝓑𝓸𝓽 -: @AbouT_AMBoT\n┣★ 𝗳𝗲𝗱𝗲𝗿𝗮𝘁𝗶𝗼𝗻𝘀 𝗝𝗼𝗶𝗻 𝗛𝗲𝗿𝗲 -:<code> /joinfed f6c21c57-962c-4858-97a2-4d2f16cd68d6</code>\n┣★ 𝙉𝙤𝙩𝙚  -: 𝗧𝗵𝗶𝘀 𝗜𝘀 𝗢𝗻𝗹𝘆 𝗙𝗼𝗿 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗙𝗼𝗿 𝗠𝘆 𝗢𝘄𝗻𝗲𝗿 {member.mention}."
+                    owner = f"#BOT_OWNER\n\n Stay Alert ⚠️\n\n{member.mention} Owner Of {app.mention} Just Joined The Group <code>{message.chat.title}</code>.\n\nSupport Me Here 👇🏻\n\n┏━━━━━━━━━━━━┓\n┣★ Updates -: @TheMenXD  \n┣★ Updates -: @Sargam_Updates \n┣★ Support -: @II_NIGHT_HIGHS_II \n┣★ Bot Username  -: @{app.username}\n┣★ 𝗦𝘂𝗽𝗲𝗿𝗯𝗮𝗻 𝗟𝗼𝗴𝘀 -: @SuperBanSBots\n┣★ About Dev -: @The_Uranium\n┣★ 𝗳𝗲𝗱𝗲𝗿𝗮𝘁𝗶𝗼𝗻𝘀 𝗝𝗼𝗶𝗻 𝗛𝗲𝗿𝗲 -:<code> /joinfed f6c21c57-962c-4858-97a2-4d2f16cd68d6</code>\n┣★ 𝙉𝙤𝙩𝙚  -: 𝗧𝗵𝗶𝘀 𝗜𝘀 𝗢𝗻𝗹𝘆 𝗙𝗼𝗿 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗙𝗼𝗿 𝗠𝘆 𝗢𝘄𝗻𝗲𝗿 {member.mention}."
                     sent_message = await message.reply_text(owner, reply_markup=buttons)
                     await asyncio.sleep(60)
                     await sent_message.delete()  
@@ -145,7 +145,7 @@ async def welcome(client, message: Message):
 
             if isinstance(SUDOERS, int): 
                 if member.id == SUDOERS:
-                    AMBOT = f"#Sudo_User\n\n Stay Alert ⚠️\n\nOf {app.mention} Sudo User {member.mention} just joined the group <code>{message.chat.title}</code>.\n\nSupport Me Here 👇🏻\n\n┏━━━━━━━━━━━━┓\n┣★ Updates -: @TheMenXD  \n┣★ Updates -: @Sargam_Updates \n┣★ Support -: @AM_YTSupport \n┣★ 𝗕𝗼𝘁 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘 -: @{app.username}\n┣★ 𝗦𝘂𝗽𝗲𝗿𝗯𝗮𝗻 𝗟𝗼𝗴𝘀 -: @SuperBanSBots\n┣★ 𝓐𝓫𝓸𝓾𝓽 𝓐𝓶𝓑𝓸𝓽 -: @AbouT_AMBoT\n┣★ 𝗳𝗲𝗱𝗲𝗿𝗮𝘁𝗶𝗼𝗻𝘀 𝗝𝗼𝗶𝗻 𝗛𝗲𝗿𝗲 -:<code> /joinfed f6c21c57-962c-4858-97a2-4d2f16cd68d6</code>."
+                    AMBOT = f"#Sudo_User\n\n Stay Alert ⚠️\n\nOf {app.mention} Sudo User {member.mention} just joined the group <code>{message.chat.title}</code>.\n\nSupport Me Here 👇🏻\n\n┏━━━━━━━━━━━━┓\n┣★ Updates -: @TheMenXD  \n┣★ Updates -: @Sargam_Updates \n┣★ Support -: @II_NIGHT_HIGHS_II \n┣★ Bot Username  -: @{app.username}\n┣★ 𝗦𝘂𝗽𝗲𝗿𝗯𝗮𝗻 𝗟𝗼𝗴𝘀 -: @SuperBanSBots\n┣★ About Dev -: @The_Uranium\n┣★ 𝗳𝗲𝗱𝗲𝗿𝗮𝘁𝗶𝗼𝗻𝘀 𝗝𝗼𝗶𝗻 𝗛𝗲𝗿𝗲 -:<code> /joinfed f6c21c57-962c-4858-97a2-4d2f16cd68d6</code>."
                     sent_message = await message.reply_text(AMBOT, reply_markup=buttons)
                     await asyncio.sleep(60)
                     await sent_message.delete()  
@@ -153,7 +153,7 @@ async def welcome(client, message: Message):
 
             elif isinstance(SUDOERS, (list, set)):
                 if member.id in SUDOERS:
-                    AMBOT = f"#Sudo_User\n\n Stay Alert ⚠️\n\nOf {app.mention} Sudo User {member.mention} just joined the group <code>{message.chat.title}</code>.\n\nSupport Me Here 👇🏻\n\n┏━━━━━━━━━━━━┓\n┣★ Updates -: @TheMenXD  \n┣★ Updates -: @Sargam_Updates \n┣★ Support -: @AM_YTSupport \n┣★ 𝗕𝗢𝗧 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘 -: @{app.username}\n┣★ 𝗦𝘂𝗽𝗲𝗿𝗕𝗮𝗻 𝗟𝗢𝗚𝗦 -: @SuperBanSBots\n┣★ 𝓐𝓫𝗼𝘂𝘁 𝓐𝗠𝗕𝗢𝗧 -: @AbouT_AMBoT\n┣★ 𝗳𝗲𝗱𝗲𝗿𝗮𝘁𝗶𝗼𝗻𝘀 𝗝𝗼𝗶𝗻 𝗛𝗲𝗿𝗲 -:<code> /joinfed f6c21c57-962c-4858-97a2-4d2f16cd68d6</code>."
+                    AMBOT = f"#Sudo_User\n\n Stay Alert ⚠️\n\nOf {app.mention} Sudo User {member.mention} just joined the group <code>{message.chat.title}</code>.\n\nSupport Me Here 👇🏻\n\n┏━━━━━━━━━━━━┓\n┣★ Updates -: @TheMenXD  \n┣★ Updates -: @Sargam_Updates \n┣★ Support -: @II_NIGHT_HIGHS_II \n┣★ Bot Username  -: @{app.username}\n┣★ 𝗦𝘂𝗽𝗲𝗿𝗕𝗮𝗻 𝗟𝗢𝗚𝗦 -: @SuperBanSBots\n┣★ About Dev -: @The_Uranium\n┣★ 𝗳𝗲𝗱𝗲𝗿𝗮𝘁𝗶𝗼𝗻𝘀 𝗝𝗼𝗶𝗻 𝗛𝗲𝗿𝗲 -:<code> /joinfed f6c21c57-962c-4858-97a2-4d2f16cd68d6</code>."
                     sent_message = await message.reply_text(AMBOT, reply_markup=buttons)
                     await asyncio.sleep(60)
                     await sent_message.delete()  
